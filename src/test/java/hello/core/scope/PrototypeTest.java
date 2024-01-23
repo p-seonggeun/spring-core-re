a04b06da9696e1ad5b63f16712d6ae573a4e21cf
psg <1999byh@naver.com>
2024-01-23 14:42:27 +0900

    프로토타입 스코프

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,38 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ProtoTypeBean.class);
+        System.out.println("find prototypeBean1");
+        ProtoTypeBean protoTypeBean1 = ac.getBean(ProtoTypeBean.class);
+        System.out.println("find prototypeBean2");
+        ProtoTypeBean protoTypeBean2 = ac.getBean(ProtoTypeBean.class);
+        System.out.println("protoTypeBean1 = " + protoTypeBean1);
+        System.out.println("protoTypeBean2 = " + protoTypeBean2);
+        assertThat(protoTypeBean1).isNotSameAs(protoTypeBean2);
+
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class ProtoTypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("ProtoTypeBean.init");
+        }
+        @PreDestroy
+        public void destroy() {
+            System.out.println("ProtoTypeBean.destroy");
+        }
+    }
+}
